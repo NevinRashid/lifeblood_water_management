79e8d008397778c44c5c710e655514973e35cc70
BsHeR4 <bshermahayni@gmail.com>
2025-07-18 10:03:14 +0300

    merge hanen branch with mine

---
+++ tests/Feature/Auth/LogoutTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_user_can_logout_successfully(): void
+    {
+        $user = User::factory()->create();
+
+        $token = $user->createToken('test_token')->plainTextToken;
+
+
+        $response = $this->postJson('api/logout', [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(204)->assertNoContent();
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'tokenable_id' => $user->id,
+            'name' => 'test_token',
+        ]);
+    }
+
+    public function test_guest_cannot_logout()
+    {
+        $response = $this->postJson('/api/logout');
+
+        $response->assertStatus(401);
+    }
+}
